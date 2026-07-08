3a33b906da57b6d7faa801e0542c3a03995c3dbf
parth-g04 <parthg0504@gmail.com>
2026-07-08 23:06:43 +0530

    chore: change frontend port to 3000 and update backend CORS settings to allow it

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS for frontend client
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000', 'http://127.0.0.1:3000'],
   credentials: true
 }));
 
